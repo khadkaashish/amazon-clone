3ce9e9ecd10ae4eac4e7ba86b78d5b699ff38b0e
khadkaashish <ashishkhadka03@gmail.com>
2023-05-07 09:47:44 +0545

    Day 1

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDbg8DpVhF6zJ4vNuWGe_oXJluMW78LShU",
+  authDomain: "clone-852f9.firebaseapp.com",
+  projectId: "clone-852f9",
+  storageBucket: "clone-852f9.appspot.com",
+  messagingSenderId: "448020158552",
+  appId: "1:448020158552:web:cd5eb947f1be1b4d5e522d",
+};
